16bc027f29ebf3b5247b656ae8bcd2db44a27595
mistermar <aydamirov92@gmail.com>
2022-05-21 21:39:56 +0700

    commit

--- index.js
+++ index.js
@@ -1,58 +1,104 @@
 const TelegramBotApi = require('node-telegram-bot-api')
+const axios = require('axios')
 
 const token = '5327266952:AAFw9CAgW__obt4gdS9aN8L1XgFJjQUF3mw'
-
 const bot = new TelegramBotApi(token, {polling: true})
 
-const stats = {}
 
-const gameOption = {
+const orderOptions = {
     reply_markup: JSON.stringify({
         inline_keyboard: [
-            [{text:1, callback_data: 'test1'}],
-            [{text:2, callback_data: 'test2'}],
-            [{text:3, callback_data: 'test3'}],
-            [{text:4, callback_data: 'test4'}],
-            [{text:5, callback_data: 'test5'}],
+            [{text:`Принять`, callback_data: `true`}, {text:`Отклонить`, callback_data: `false`}],
         ]
     })
 }
 
-function start (){
-    bot.setMyCommands([
-        {command: '/start', description: "hello here"},
-        {command: '/info', description: "info here"},
-        {command: '/game', description: "info game"}
-    ]).then( r => {
-        console.log(77777,r)
-    })
+function orderOption(status) {
+    let text = status ? 'Принято' : 'Отклонено'
+  return {
+      reply_markup: JSON.stringify({
+          inline_keyboard: [
+              [{text: text, callback_data: `test`}],
+          ]
+      })
+  }
+}
+function orderOption2(status) {
+    let text = status ? 'Принято' : 'Отклонено'
+  return {
+      reply_markup: JSON.stringify({
+          inline_keyboard: [
+              [{text: '31', callback_data: `test`}],
+          ]
+      })
+  }
+}
 
-    bot.on('message', async (msg)=>{
+let orders = [
+    {name: 'pizza'},
+    {name: 'tea'},
+    {name: 'roll'},
+    {name: 'bread'},
+]
+
+async function orderFn(chatId, orders){
+    if(!orders.length){
+        await bot.sendMessage(chatId, `All done! Waiting new orders`)
+
+        return false;
+    }
+
+    bot.sendMessage(chatId, `order name ${orders[0].name}`, orderOptions)
+}
+
+
+function start (){
+
+    bot.setMyCommands([
+        {command: '/start', description: "start msg"},
+        {command: '/info', description: "info name"},
+        {command: '/order', description: "click"},
+        {command: '/add_orders', description: "click to add"},
+    ])
+
+    bot.on('message', async (msg) => {
         const chatId = msg.chat.id
         const text = msg.text
+        const userId = msg.from.id
 
+        if(text === '/order'){
+            await bot.sendMessage(chatId, `You have new orders count : ${orders.length}`)
 
-        if(text === '/start'){
-            await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.jpg')
-            await bot.sendMessage(chatId, 'Hi '+ msg.from.username + " " + msg.from.first_name)
+            orderFn(chatId, orders)
+            return true;
         }
+
         if(text === '/info'){
             await bot.sendMessage(chatId, 'your name '+ msg.from.username + " " + msg.from.first_name)
         }
-        if(text === '/game'){
-            await bot.sendMessage(chatId, 'угадай число')
-            const answer = 5
-            stats[chatId] = answer
-            return bot.sendMessage(chatId, 'угадай число 5', gameOption)
+        if(text === '/add_orders'){
+            orders.push({name: 'Super Pizza'})
+            return true;
         }
+
 
         return bot.sendMessage(chatId, 'повторите попытку')
     })
 
-    bot.on("callback_query", msg=>{
-        console.log(msg)
+    bot.on("callback_query", async msg=>{
+        const chatId = msg.message.chat.id
+        if(msg.data === 'true'){
+            await bot.sendMessage(chatId, `  принято`)
+            orders.splice(0,1)
+            orderFn(chatId, orders)
+
+        }else if (msg.data === 'false'){
+            await bot.sendMessage(chatId, `отменено`)
+            orders.splice(0,1)
+
+            orderFn(chatId, orders)
+        }
     })
 }
-
 
 start()
